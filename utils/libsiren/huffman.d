b762f0afa06c316c1a21ef20e9bba849dd85b8f3
deadcow <deadcow@mosdef.org>
2009-01-16 14:23:10 -0200

    update repo

--- utils/libsiren/huffman.d
+++
@@ -1,4 +0,0 @@
-utils/libsiren/huffman.o: utils/libsiren/huffman.c \
-  utils/libsiren/siren7.h utils/libsiren/encoder.h utils/libsiren/dct4.h \
-  utils/libsiren/rmlt.h utils/libsiren/huffman.h utils/libsiren/decoder.h \
-  utils/libsiren/common.h utils/libsiren/huffman_consts.h
--- utils/msgpluslive.d
+++
@@ -1,1 +0,0 @@
-utils/msgpluslive.o: utils/msgpluslive.c
